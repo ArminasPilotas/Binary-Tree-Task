258851dd9f74f5a9b942675fe52b070ffe2988ce
ArminasPilotas <arminas.pilotas@gmail.com>
2021-01-26 22:33:50 +0200

    Initial commit

---
+++ NodeReader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Matrix_Software_Task
+{
+   public class NodeReader
+    {
+        public readonly String path = Path.Combine(Directory.GetCurrentDirectory(), "text.txt");
+        public string[] text { get; private set; }
+
+        public NodeReader()
+        {
+            ReadFile();
+        }
+        private void ReadFile()
+        {
+            text = System.IO.File.ReadAllLines(path);
+        }
+       
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Matrix_Software_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            NodeWritter nodeWritter = new NodeWritter();
+            nodeWritter.WriteToNode();
+            PathFinder pathFinder = new PathFinder(nodeWritter);
+
+            
+            Console.Write("Path: {0}",string.Join(",",pathFinder.GetBiggestPath()));
+            Console.WriteLine();
+            Console.WriteLine("Sum : {0}", pathFinder.largestPathValue);
+            Console.WriteLine("All possible routes: {0}", pathFinder.maxRoutes);
+        }
+        
+
+    }
+}
